f93f2747ff37ddf5c74bba933a268beec5b2fede
Grek <grrekov@yandex.ru>
2020-04-01 23:04:41 +0300

    Добавьте файлы проекта.

---
+++ MyFirstWPF1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace MyFirstWPF1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Confirm_Click(object sender, RoutedEventArgs e)
+        {
+            if (Second.Text == "grek" && Third.Text == "123")
+            {
+                this.Cursor = Cursors.Wait;
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                MessageBox.Show("Добро пожаловать на сайт, дорогой , " + Second.Text);
+            }
+            else
+            {
+                MessageBox.Show("Вы ввели неправильный пароль.");
+            }
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
